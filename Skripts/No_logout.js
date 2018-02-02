6cc372e19be9ef7da0b3574281f8106f436853c2
Stomson <1425157@stud.hs-mannheim.de>
2018-02-02 12:26:02 +0100

    Create No_logout.js

---
+++ Skripts/No_logout.js
@@ -0,0 +1,6 @@
+// ==UserScript==
+// @name      No_Logout
+// @namespace *
+// @include 	[URL DER SEITE]/*/*
+// ==/UserScript==
+setInterval("window.timerPruefeAnmeldung = window.setTimeout('pruefeAnmeldung();', 9^9999);", 2000);
